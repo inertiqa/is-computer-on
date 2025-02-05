b0ed0c79c9b241b5f47609ce54bb8d4b9ef4225b
ᑐᑌᑎᕮ <ooqq@uoc.edu>
2025-02-05 21:11:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*!
- * is-computer-on <https://github.com/necronomimicon/is-computer-on>
- *
- * Copyright (c) ネクローノーミミコン.
- * Released under the MIT License.
- */
-
 'use strict';
 
 module.exports = function isComputerOn() {
